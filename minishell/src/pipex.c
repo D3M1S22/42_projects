424c4e1000924a4ded0cfef3554f7b17be95bd8c
D3M1S22 <damianoshushku@gmail.com>
2024-01-26 01:10:02 +0100

    edited pipex.c

--- minishell/src/pipex.c
+++ minishell/src/pipex.c
@@ -6,13 +6,15 @@
 /*   By: dshushku <dshushku@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 15:14:43 by dshushku          #+#    #+#             */
-/*   Updated: 2024/01/25 16:48:08 by dshushku         ###   ########.fr       */
+/*   Updated: 2024/01/26 01:09:36 by dshushku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <malloc/_malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/_types/_null.h>
 #include <unistd.h>
 
 t_cmd	**check_cmds(t_cmd **cmds, int ncmds)
@@ -41,6 +43,17 @@ t_cmd	**malloc_cmds(size_t size)
 	return (tmp);
 }
 
+int ft_handle_access(t_cmd *cmd, char *command, char *tmp){
+	cmd->cmd = ft_strdup(command);
+	cmd->error = 0;
+	free_parse(tmp, command);
+	if (access(cmd->cmd, 0) == 0)
+		return (1);
+	cmd->error = 1;
+	return 0;
+}
+
+
 t_cmd	**ft_parse_cmd(char **argv, int ncmds, char **paths)
 {
 	t_cmd	**cmds;
@@ -60,12 +73,8 @@ t_cmd	**ft_parse_cmd(char **argv, int ncmds, char **paths)
 		{
 			tmp = ft_strjoin(paths[j], "/");
 			command = ft_strjoin(tmp,cmds[i]->f_cmd[0]);
-			cmds[i]->cmd = ft_strdup(command);
-			cmds[i]->error = 0;
-			free_parse(tmp, command);
-			if (access(cmds[i]->cmd, 0) == 0)
-				break ;
-			cmds[i]->error = 1;
+			if(ft_handle_access(cmds[i], command, tmp))
+				break;
 		}
 	}
 	return (check_cmds(cmds, ncmds));
@@ -76,6 +85,86 @@ char	**ft_parse_paths(char **envp)
 	while (ft_strncmp("PATH", *envp, 4))
 		envp++;
 	return (ft_split(*envp, ':'));
+}
+
+void tokenize_redir(t_cmd *cmd)
+{
+	int j;
+	int i;
+
+	j = 0;
+	while (cmd->f_cmd[j] != (void *)0)
+		j++;	
+	cmd->redirection = malloc((j + 1) * sizeof(char *));
+	j = 0;
+	i = -1;
+	while(cmd->f_cmd[++j] != (void *)0){
+		if(cmd->f_cmd[j][0] == '\'' || cmd->f_cmd[j][0] == '\"')
+			break;
+		else if(ft_strncmp(cmd->f_cmd[j], "<<", 2) == 0){
+			cmd->redirection[++i] = ft_strdup(cmd->f_cmd[j]);
+		}
+		else if(ft_strncmp(cmd->f_cmd[j], "<", 1) == 0){
+			write(1, "2\n",2);
+			cmd->redirection[++i] = ft_strdup(cmd->f_cmd[j]);
+		}
+		else if (ft_strncmp(cmd->f_cmd[j], ">>", 2) == 0){
+			printf("3\n");
+			cmd->redirection[++i] = ft_strdup(cmd->f_cmd[j]);
+		}
+		else if (ft_strncmp(cmd->f_cmd[j], ">", 1) == 0){
+			printf("4\n");
+			cmd->redirection[++i] = ft_strdup(cmd->f_cmd[j]);
+		}
+		else{
+			// cmd->redirection[++i] = NULL;
+		}
+	}
+}
+
+void ft_test(char *cmd){
+	printf("%s\n", cmd);
+}
+
+void tokenize_builtin(t_cmd *cmd)
+{
+	int j;
+	int i;
+
+	i = 0;
+	j = 0;
+	while(cmd->f_cmd[++j] != (void *)0)
+		if(cmd->f_cmd[j][0] == '\'' || cmd->f_cmd[j][0] == '\"')
+			break;
+		else if(ft_strncmp(cmd->f_cmd[j], "cd", 2) == 0)
+			cmd->builtin = ft_test;
+		else if(ft_strncmp(cmd->f_cmd[j], "echo", 4) == 0)
+			cmd->builtin = ft_test;
+		else if (ft_strncmp(cmd->f_cmd[j], "pwd", 3) == 0)
+			cmd->builtin = ft_test;
+		else if (ft_strncmp(cmd->f_cmd[j], "exit", 4) == 0)
+			cmd->builtin = ft_test;
+		else if (ft_strncmp(cmd->f_cmd[j], "export", 6) == 0)
+			cmd->builtin = ft_test;
+		else if (ft_strncmp(cmd->f_cmd[j], "unset", 5) == 0)
+			cmd->builtin = ft_test;
+		else if (ft_strncmp(cmd->f_cmd[j], "env", 3) == 0)
+			cmd->builtin = ft_test;
+		else
+			cmd->builtin = NULL;
+}
+
+void tokenizer(t_cmd **cmds, int n_cmds)
+{
+	int i;
+	int j;
+
+	i = -1;
+	while (++i < n_cmds) {
+		tokenize_redir(cmds[i]);
+		// tokenize_builtin(cmds[i]);
+	}
+
 }
 
 void	pipex(t_pipex *pipex, const char *argv, char **envp)
@@ -91,18 +180,29 @@ void	pipex(t_pipex *pipex, const char *argv, char **envp)
 	pipex->cmd_paths = ft_parse_paths(envp);
 	pipex->cmds = ft_parse_cmd(piped_str,
 			pipex->n_cmds, pipex->cmd_paths);
+	tokenizer(pipex->cmds, pipex->n_cmds);
+	
+
+
 	int j = -1;
 	while (++j<i) {
-		printf("pipex->cmds[%d] = %s\n, isError %d ", j, pipex->cmds[j]->cmd,pipex->cmds[j]->error);
+		// printf("pipex->cmds[%d] = %s\n, isError %d ", j, pipex->cmds[j]->cmd,pipex->cmds[j]->error);
 		int k = -1;
-		while (pipex->cmds[j]->f_cmd[++k]!= (void *)0)
-			printf("pipex->cmds[%d]->f_cmd[%d] =  %s\n",j, k, pipex->cmds[j]->f_cmd[k]);
+		while (pipex->cmds[j]->f_cmd[++k]!= (void *)0){
+			int m = -1;
+			while (pipex->cmds[j]->redirection[++m] != ){
+				printf("D\n");
+				printf("%s\n", pipex->cmds[j]->redirection[m]);
+			}
+		}
+			// printf("pipex->cmds[%d]->f_cmd[%d] =  %s\n",j, k, pipex->cmds[j]->f_cmd[k]);
 	}
-	i=0;
-	while (piped_str[i] != (void *)0) {
-		free(piped_str[i]);
-	}
-	free(piped_str);
+	
+	// i=0;
+	// while (piped_str[i] != (void *)0) {
+	// 	free(piped_str[i]);
+	// }
+	// free(piped_str);
 	// int e = execve(pipex->cmds[0]->cmd, pipex->cmds[0]->f_cmd, envp);
 	// printf("execve res : %d\n", e);
 	// return ;
